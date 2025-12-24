73cddcc08d9e776ca29ba95d1e18235871224627
Chris Kevini <chriskevini@gmail.com>
2025-12-24 03:53:05 -0800

    fix: let production deployment use remote server's N8N_API_KEY instead of passing local one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -186,7 +186,6 @@ deploy_prod() {
                 "$N8N_DEV_SSH_HOST:/opt/kairon/scripts/workflows/"
             
             ssh "$N8N_DEV_SSH_HOST" "cd /opt/kairon && \
-                N8N_API_KEY='$N8N_API_KEY' \
                 WORKFLOW_DIR='/opt/kairon/n8n-workflows' \
                 bash /opt/kairon/scripts/workflows/n8n-push-prod.sh" > "$OUTPUT_FILE" 2>&1
         else
